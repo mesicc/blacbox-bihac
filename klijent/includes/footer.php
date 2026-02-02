3789c91f9a769a45638cd44b5a117b8b83fc4f8e
Kemal Mešić <153745485+mesicc@users.noreply.github.com>
2026-02-02 10:26:00 +0100

    Napravljena responzivnost na index.php i rezervacije.php, dodaan klijent.css

--- klijent/includes/footer.php
+++ klijent/includes/footer.php
@@ -1,5 +1,24 @@
             </div>
         </main>
     </div>
+    <script>
+        const btn = document.getElementById('hamburgerBtn');
+        const sidebar = document.querySelector('.klijent-sidebar');
+        const overlay = document.getElementById('sidebarOverlay');
+
+        if(btn) {
+            btn.onclick = () => {
+                sidebar.classList.toggle('show');
+                overlay.classList.toggle('active');
+            };
+        }
+
+        if(overlay) {
+            overlay.onclick = () => {
+                sidebar.classList.remove('show');
+                overlay.classList.remove('active');
+            };
+        }
+    </script>
 </body>
 </html>
